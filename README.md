3bcf1e8cf52e2abe94d2c3a4b4da2bd27cc7a929
Alphons Jaimon <jaimonalphons@gmail.com>
2020-04-28 03:06:11 +0530

    modified

---
+++ README.md
@@ -0,0 +1,7 @@
+# Fortune (fortune.py)
+A super simple implementation of the famous unix fortune
+
+### Depends on the fortune.json file!
+Add you own quotes to it manually in the fortunes.json file
+
+Already contains more than 2000 quotes!
--- fortune.py
+++ fortune.py
@@ -2,15 +2,13 @@ import random
 import json
 from colorama import Fore
 
-def fortune():
-    try:
-        json_file = open('fortunes.json')
-    except:
-        print(Fore.RED, "File not found!")
-        return
-    json_data = json.load(json_file)
-    fortunes = json_data['data']
-    x = random.randrange(2335)
-    print("\"", fortunes[x]['quote'],"\"")
-    print(" - ",fortunes[x]['author'])
-    
+try:
+    json_file = open('fortunes.json')
+except:
+    print(Fore.RED, "File not found!")
+    return
+json_data = json.load(json_file)
+fortunes = json_data['data']
+x = random.randrange(len(fortunes[:]))
+print("\"", fortunes[x]['quote'],"\"")
+print(" - ",fortunes[x]['author'])
